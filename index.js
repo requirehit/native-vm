e13ca8a27d226e7eaadeff6e40bbb07ff94d6d11
José Moreira <jmoreira@microdual.com>
2015-09-18 14:13:58 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+var global = (function () { return this; })();
+var Script = require( './lib/script.js' );
+
+var util = require('util');
+
+// The binding provides a few useful primitives:
+// - ContextifyScript(code, { filename = "evalmachine.anonymous",
+//                            displayErrors = true } = {})
+//   with methods:
+//   - runInThisContext({ displayErrors = true } = {})
+//   - runInContext(sandbox, { displayErrors = true, timeout = undefined } = {})
+// - makeContext(sandbox)
+// - isContext(sandbox)
+// From this we build the entire documented API.
+
+exports.createScript = function(code, options) {
+  return new Script(code, options);
+};
+
+exports.createContext = function(sandbox) {
+  if (util.isUndefined(sandbox)) {
+    sandbox = {};
+  } else if (binding.isContext(sandbox)) {
+    return sandbox;
+  }
+
+  binding.makeContext(sandbox);
+  return sandbox;
+};
+
+exports.runInDebugContext = function(code) {
+  return binding.runInDebugContext(code);
+};
+
+exports.runInContext = function(code, contextifiedSandbox, options) {
+  var script = new Script(code, options);
+  return script.runInContext(contextifiedSandbox, options);
+};
+
+exports.runInNewContext = function(code, sandbox, options) {
+  var script = new Script(code, options);
+  return script.runInNewContext(sandbox, options);
+};
+
+exports.runInThisContext = function(code, options) {
+  var script = new Script(code, options);
+  return script.runInThisContext(options);
+};
+
+exports.isContext = Script.isContext;
